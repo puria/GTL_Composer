0ac115753d14d51346057c19f411b61aaf317dce
Puria Nafisi Azizi <puria@dyne.org>
2019-12-12 09:44:05 +0100

    💄 Adding style with correct glyph font and related transformations

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+const table = {'a': {'structure': "[('rect', 0, False)]", 'index': 0}, 'b': {'structure': "[('rect', 90, False)]", 'index': 1}, 'c': {'structure': "[('rect', 180, False)]", 'index': 2}, 'd': {'structure': "[('rect', 270, False)]", 'index': 3}, 'e': {'structure': "[('rect', 0, False), ('rect', 180, False)]", 'index': 0}, 'f': {'structure': "[('rect', 90, False), ('rect', 270, False)]", 'index': 1}, 'g': {'structure': "[('rect', 0, False), ('rect', 90, False)]", 'index': 0}, 'h': {'structure': "[('rect', 90, False), ('rect', 180, False)]", 'index': 1}, 'i': {'structure': "[('rect', 180, False), ('rect', 270, False)]", 'index': 2}, 'j': {'structure': "[('rect', 270, False), ('rect', 0, False)]", 'index': 3}, 'k': {'structure': "[('rect', 0, False), ('rect', 90, False), ('rect', 180, False)]", 'index': 0}, 'l': {'structure': "[('rect', 90, False), ('rect', 180, False), ('rect', 270, False)]", 'index': 1}, 'm': {'structure': "[('rect', 180, False), ('rect', 270, False), ('rect', 0, False)]", 'index': 2}, 'n': {'structure': "[('rect', 270, False), ('rect', 0, False), ('rect', 90, False)]", 'index': 3}, 'o': {'structure': "[('rect', 0, False), ('rect', 90, False), ('rect', 180, False), ('rect', 0, False)]", 'index': 0}, 'p': {'structure': "[('rect', 0, False), ('diag_0', 0, True)]", 'index': 0}, 'q': {'structure': "[('rect', 90, False), ('diag_0', 90, True)]", 'index': 1}, 'r': {'structure': "[('rect', 180, False), ('diag_0', 180, True)]", 'index': 2}, 's': {'structure': "[('rect', 270, False), ('diag_0', 270, True)]", 'index': 3}, 't': {'structure': "[('rect', 0, False), ('diag_0', 0, False)]", 'index': 0}, 'u': {'structure': "[('rect', 90, False), ('diag_0', 90, False)]", 'index': 1}, 'v': {'structure': "[('rect', 180, False), ('diag_0', 180, False)]", 'index': 2}, 'w': {'structure': "[('rect', 270, False), ('diag_0', 270, False)]", 'index': 3}, 'x': {'structure': "[('diag_0', 0, True)]", 'index': 0}, 'y': {'structure': "[('diag_0', 90, True)]", 'index': 1}, 'z': {'structure': "[('diag_0', 180, True)]", 'index': 2}, 'A': {'structure': "[('diag_0', 270, True)]", 'index': 3}, 'B': {'structure': "[('diag_0', 0, False)]", 'index': 0}, 'C': {'structure': "[('diag_0', 90, False)]", 'index': 1}, 'D': {'structure': "[('diag_0', 180, False)]", 'index': 2}, 'E': {'structure': "[('diag_0', 270, False)]", 'index': 3}, 'F': {'structure': "[('diag_1', 0, False), ('diag_1', 180, False)]", 'index': 0}, 'G': {'structure': "[('diag_1', 90, False), ('diag_1', 270, False)]", 'index': 1}, 'H': {'structure': "[('diag_1', 0, False), ('diag_1', 180, False), ('diag_1', 180, True)]", 'index': 0}, 'I': {'structure': "[('diag_1', 90, False), ('diag_1', 270, False), ('diag_1', 270, True)]", 'index': 1}, 'J': {'structure': "[('diag_1', 0, True), ('diag_1', 180, True), ('diag_1', 180, False)]", 'index': 0}, 'K': {'structure': "[('diag_1', 90, True), ('diag_1', 270, True), ('diag_1', 270, False)]", 'index': 1}, 'L': {'structure': "[('diag_1', 0, False), ('diag_1', 180, False), ('diag_1', 180, True), ('diag_1', 0, True)]", 'index': 0}, 'M': {'structure': "[('rect', 0, False), ('rect', 180, False), ('diag_1', 0, False)]", 'index': 0}, 'N': {'structure': "[('rect', 90, False), ('rect', 270, False), ('diag_1', 90, False)]", 'index': 1}, 'O': {'structure': "[('rect', 180, False), ('rect', 0, False), ('diag_1', 180, False)]", 'index': 2}, 'P': {'structure': "[('rect', 270, False), ('rect', 90, False), ('diag_1', 270, False)]", 'index': 3}, 'Q': {'structure': "[('rect', 0, False), ('diag_1', 180, False), ('diag_1', 180, True)]", 'index': 0}, 'R': {'structure': "[('rect', 90, False), ('diag_1', 270, False), ('diag_1', 270, True)]", 'index': 1}, 'S': {'structure': "[('rect', 180, False), ('diag_1', 0, False), ('diag_1', 0, True)]", 'index': 2}, 'T': {'structure': "[('rect', 270, False), ('diag_1', 90, False), ('diag_1', 90, True)]", 'index': 3}, 'U': {'structure': "[('rect', 0, False), ('rect', 180, False), ('diag_1', 0, True), ('diag_1', 180, True)]", 'index': 0}, 'V': {'structure': "[('rect', 90, False), ('rect', 270, False), ('diag_1', 90, True), ('diag_1', 270, True)]", 'index': 1}, 'W': {'structure': "[('rect', 180, False), ('rect', 0, False), ('diag_1', 180, True), ('diag_1', 0, True)]", 'index': 2}, 'X': {'structure': "[('rect', 270, False), ('rect', 90, False), ('diag_1', 270, True), ('diag_1', 90, True)]", 'index': 3}, 'Y': {'structure': "[('rect', 0, False), ('rect', 90, False), ('rect', 180, False), ('diag_1', 0, True)]", 'index': 0}, 'Z': {'structure': "[('rect', 90, False), ('rect', 180, False), ('rect', 270, False), ('diag_1', 90, True)]", 'index': 1}, '0': {'structure': "[('rect', 180, False), ('rect', 270, False), ('rect', 0, False), ('diag_1', 180, True)]", 'index': 2}, '1': {'structure': "[('rect', 270, False), ('rect', 0, False), ('rect', 90, False), ('diag_1', 270, True)]", 'index': 3}, '2': {'structure': "[('rect', 0, False), ('rect', 180, False), ('diag_0', 0, True), ('diag_0', 180, True)]", 'index': 0}, '3': {'structure': "[('rect', 90, False), ('rect', 270, False), ('diag_0', 90, True), ('diag_0', 270, True)]", 'index': 1}, '4': {'structure': "[('rect', 180, False), ('rect', 0, False), ('diag_0', 180, True), ('diag_0', 0, True)]", 'index': 2}, '5': {'structure': "[('rect', 270, False), ('rect', 90, False), ('diag_0', 270, True), ('diag_0', 90, True)]", 'index': 3}}
+
 let props = {
   unicode_name: '',
-  rows: 10,
-  lines: 10,
+  rows: 7,
+  lines: 7,
   mouseover: false,
-  characters: '│┤└┴┬├─┼┘┌┐/\\',
+  characters: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345',
   vertical_stretch: false,
   horizontal_stretch: false,
 };
@@ -36,7 +38,7 @@ function dot_paint(e) {
   let sign = $("input[name=char]:checked").val();
   var content = $(this).text();
   if (e.which==1)
-    $(this).text(content !== "·" ? "·" : sign);
+    $(this).text(content !== "." ? "." : sign);
 }
 
 function draw_grid() {
@@ -47,7 +49,11 @@ function draw_grid() {
   for (var line = 0; line < lines; line++) {
     $("#glyph").append($("<br>"));
     for (var row = 0; row < rows; row++) {
-      $("#glyph").append($(`<code data-i='${row}' data-j='${line}'>·</code>`));
+      var cls = ""
+      if (row==0 || row==rows-1 || line==0 || line==lines-1)
+         cls = "margin_area"
+
+      $("#glyph").append($(`<code class="${cls}" data-i='${row}' data-j='${line}'>.</code>`));
     }
   }
 
@@ -62,7 +68,7 @@ function attach_paint_event() {
 }
 
 function generate_char_radio(char) {
-    $("#chars").append($(`<div>
+    $("#chars").append($(`<div class='radio_container'>
     <input type="radio" id="${char}" name="char" value="${char}">
     <label for="sharp">${char}</label>
     </div>`))
@@ -82,11 +88,13 @@ function export_glyph() {
   o.legacy_text = legacy
   o.rows = props.rows
   o.lines = props.lines
-  o.modules = []
+  o.cells = []
 
   $("code").each(function(index) {
     let metadata = $(this).data()
-    o.modules.push(metadata)
+    metadata.char = $(this).text()
+    metadata.transformation = table[metadata.char]
+    o.cells.push(metadata)
   })
 
   return o;
@@ -106,13 +114,13 @@ $(document).ready(function() {
   var gui = new dat.GUI();
   gui.add(props, "unicode_name")
   var rows_controller = gui.add(props, "rows");
-  var lines_controller = gui.add(props, "lines");
+  //var lines_controller = gui.add(props, "lines");
   var mouseover_controller = gui.add(props, "mouseover")
   var chars_controller = gui.add(props, "characters");
   var vertical_stretch_controller = gui.add(props, "vertical_stretch")
   var horizontal_stretch_controller = gui.add(props, "horizontal_stretch")
   rows_controller.onFinishChange(draw_grid)
-  lines_controller.onFinishChange(draw_grid)
+  //lines_controller.onFinishChange(draw_grid)
   mouseover_controller.onChange(attach_paint_event)
   chars_controller.onFinishChange(generate_radio)
   horizontal_stretch_controller.onChange(add_x_stetch)
@@ -122,14 +130,5 @@ $(document).ready(function() {
   $("#copy").on("click", function() {
     console.log(export_glyph())
     download()
-    // var $temp = $("<textarea>");
-    // $("body").append($temp);
-    // var tbc = $("#glyph")
-    //   .html()
-    //   .replace(/<br>/gi, "\r\n")
-    //   .replace(/<[^>]*>/g, "");
-    // $temp.val(tbc).select();
-    // document.execCommand("copy");
-    // $temp.remove();
   });
 });
